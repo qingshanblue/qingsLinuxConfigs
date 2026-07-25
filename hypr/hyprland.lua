b7e099d5df6ff99aa11adaf88d03084870bac3a4
qingshanblue <qingshanblue@gmail.com>
2026-07-25 20:13:48 +0800

    更改runner为anyrun

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -25,7 +25,7 @@ hl.monitor({
 -- Set programs that you use
 local terminal    = "kitty"
 local fileManager = "dolphin"
-local menu        = "hyprlauncher"
+local menu        = "anyrun"
 
 -------------------
 ---- AUTOSTART ----
